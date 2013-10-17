c9f63cdb4003adafb72d13b6673b8e64760c137f
taptap <alexey.e.egorov@gmail.com>
2013-10-17 16:43:53 +0400

    fix

--- prims_algorithm.ml
+++ prims_algorithm.ml
@@ -1,10 +1,20 @@
-let build_maze w h =
+let print_border w h =
+  for i = 1 to h + 2 do
+    print_char '#'
+  done;
+  print_char '\n';
   for i = 1 to w do
-    for h = 1 to h do
-      print_char '*'
+    print_char '#';
+    for j = 1 to h do
+      print_char ' '
     done;
+    print_char '#';
     print_char '\n'
-  done;;
+  done;
+  for i = 1 to h + 2 do
+    print_char '#'
+  done;
+  print_char '\n';;  
 
 let () =
   if Array.length Sys.argv < 3 then
@@ -12,5 +22,5 @@ let () =
   else
     let width = int_of_string Sys.argv.(1) and
 	height = int_of_string Sys.argv.(2) in
-    build_maze width height;;
+    print_border width height;;
       
