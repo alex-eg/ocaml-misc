1113cad1c3e80470a174e3425ae31d5d82cff1c2
taptap <alexey.e.egorov@gmail.com>
2013-10-18 11:18:49 +0400

    add stub generate_maze function

--- prims_algorithm.ml
+++ prims_algorithm.ml
@@ -1,3 +1,6 @@
+let generate_maze w h =
+  "stub";;
+  
 let init_maze w h =
   let res = Array.make w (Array.make h 0) in
   for i = 0 to w - 1 do
@@ -5,7 +8,7 @@ let init_maze w h =
   done;
   res;;
 
-let generate_maze w h =
+let print_maze (maze, w, h) =
   for i = 1 to h + 2 do
     print_char '#'
   done;
@@ -29,5 +32,5 @@ let () =
   else
     let width = int_of_string Sys.argv.(1) and
 	height = int_of_string Sys.argv.(2) in
-    generate_maze width height;;
+    print_maze (generate_maze width height, width, height);;
       
